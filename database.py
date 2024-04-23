715e2ebd54f6062bc15b19139339f3c21db5c82d
yejue <1145331931@qq.com>
2024-04-23 10:52:48 +0800

    [feat] 引入 alembic 数据库迁移管理

--- database.py
+++ database.py
@@ -11,7 +11,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 BaseModel = declarative_base()
-BaseModel.metadata.create_all(bind=engine)
 
 
 def get_db():
